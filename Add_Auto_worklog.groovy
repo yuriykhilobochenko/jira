e76924697ffb58ed97f908dfa2fc021e229cc7dc
yuriykhilobochenko <reeperh@outlook.com>
2017-04-06 13:11:48 +0300

    Update Add_Auto_worklog.groovy

--- Add_Auto_worklog.groovy
+++ Add_Auto_worklog.groovy
@@ -1,7 +1,8 @@
 import com.atlassian.jira.issue.worklog.WorklogImpl
 import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.sal.api.component.ComponentLocator
-    
+import java.util.Date
+import java.sql.Date
    import com.atlassian.jira.issue.fields.DefaultFieldManager
 import com.atlassian.sal.api.component.ComponentLocator
 import com.atlassian.jira.issue.IssueImpl
@@ -13,8 +14,9 @@ import com.atlassian.jira.util.JiraDurationUtils
 def fieldManager = ComponentLocator.getComponent(DefaultFieldManager.class)
 def issueService = ComponentLocator.getComponent(DefaultIssueService.class)
 def userManager = ComponentAccessor.getUserManager()
+def currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
 
-def customValue = fieldManager.getCustomField("customfield_10202")
+def customValue = fieldManager.getCustomField("customfield_11108")
 
 def worklogManager = ComponentAccessor.getWorklogManager()
 double timesum = 0
@@ -25,28 +27,30 @@ worklogManager.getByIssue(issue).each() {
     timesum += issue.getTimeSpent();
 }
 
-long time = issue.getCustomFieldValue(customValue).toString().toLong()
+//long time = issue.getCustomFieldValue(customValue).toString().toLong()
 
 
-long today = System.currentTimeMillis().toLong()
-long daysBetween = (today - time)
-issue.setTimeSpent(daysBetween)
-
-issue.setCustomFieldValue(customValue,System.currentTimeMillis().toString())
-
+//long today = System.currentTimeMillis().toLong()
+long daysBetween = ((System.currentTimeMillis().toLong() - issue.getCustomFieldValue(customValue).toString().toLong()) / 1000).toLong();
+timesum += daysBetween
+//Date dat = Date.valueOf(issue.getCustomFieldValue(customValue).toString())
+Date d = new Date(issue.getCustomFieldValue(customValue).toString().toLong())
+Date now = new Date(System.currentTimeMillis())
+//issue.setTimeSpent(issue.getTimeSpent() + daysBetween)
 
    WorklogImpl worklog = new WorklogImpl(worklogManager,
                                          issue,
                                          null,
-                                         issue.reporter.name,
+                                         currentUser.name,
                                          issue.summary,
-                                         issue.getUpdated(),
+                                         d,
                                          null,
                                          null,
                                          daysBetween,
-                                         issue.reporter.name,
-                                         issue.getUpdated()-10,
-                                         issue.getUpdated())   
+                                         currentUser.name,
+                                         now,
+                                         null)   
 
-    worklogManager.create(issue.reporter, worklog, 0L, true)
+    worklogManager.create(currentUser, worklog, 1L,false)
 
+issue.setCustomFieldValue(customValue,System.currentTimeMillis().toString())
